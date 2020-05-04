2711f65dd242e7ae042652c10e8ca2a67440c34b
mini <starzzangna@naver.com>
2020-05-04 15:15:45 +0900

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-'use strict';
-module.exports = {
-	// publicPath: process.env.NODE_ENV === 'production' ? '/project/' : '/',
-	outputDir: 'docs'
-};
+// 'use strict';
+// module.exports = {
+// 	// publicPath: process.env.NODE_ENV === 'production' ? '/project/' : '/',
+// 	outputDir: 'docs'
+// };
